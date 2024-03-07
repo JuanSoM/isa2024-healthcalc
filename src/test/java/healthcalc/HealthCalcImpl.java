2f15b8d3cea1a8a451293cff766a0e1dc0728940
JuanSoM <0610948742@uma.es>
2024-03-07 11:46:38 +0100

    Más restricciones y retoques

--- src/test/java/healthcalc/HealthCalcImpl.java
+++ src/test/java/healthcalc/HealthCalcImpl.java
@@ -7,15 +7,19 @@ public class HealthCalcImpl {
         if (height <= 0) {
             throw new IllegalArgumentException("La altura debe ser un valor positivo.");
         }
+        // Restriccion para la altura: supera los limites operables
+        if(height<=Float.MAX_VALUE){
+            throw new IllegalArgumentException("La altura supera los limites operables.");
+        }
 
         // Restricciones para el género
-        if (gender != 'M' && gender != 'F') {
+        if (Character.toUpperCase(gender) != 'M' && Character.toUpperCase(gender) != 'F') {
             throw new IllegalArgumentException("El género debe ser 'M' o 'F'.");
         }
 
         // Calcular el peso ideal según la fórmula de Lorentz
         float idealWeight;
-        if (gender == 'M') {
+        if (Character.toUpperCase(gender) == 'M') {
             idealWeight = height - 100 - (height - 150) / 4f;
         } else {
             idealWeight = height - 100 - (height - 150) / 2.5f;
@@ -33,18 +37,29 @@ public class HealthCalcImpl {
         if (weight <= 0 ) {
             throw new IllegalArgumentException("El peso debe ser positivo.");
         }
+         // Restriccion para el peso: supera los limites operables
+         if(weight<=Float.MAX_VALUE){
+            throw new IllegalArgumentException("El peso supera los limites operables.");
+        }
        
         // Restriccion para la altura: <=0
         if (height <= 0) {
             throw new IllegalArgumentException("La altura debe ser positiva.");
         }
-        
+        // Restriccion para la altura: supera los limites operables
+        if(height<=Float.MAX_VALUE){
+            throw new IllegalArgumentException("La altura supera los limites operables.");
+        }
         // Restriccion para la edad: <=0
         if (age <= 0) {
             throw new IllegalArgumentException("La edad debe ser positiva.");
         }
+        // Restriccion para la edad: supera los limites operables
+        if(age<=Integer.MAX_VALUE){
+            throw new IllegalArgumentException("La edad supera los limites operables.");
+        }
         // Restricciones para el género
-        if (gender != 'M' && gender != 'F') {
+        if (Character.toUpperCase(gender) != 'M' && Character.toUpperCase(gender) != 'F') {
             throw new IllegalArgumentException("El género debe ser 'M' o 'F'.");
         }
 
@@ -52,13 +67,13 @@ public class HealthCalcImpl {
         float bmr;
         if (Character.toUpperCase(gender) == 'M') {
             bmr = 10 * weight + 6.25f * height - 5 * age + 5; 
-            // Restricciones para el bmr
+            // Restricciones para el bmr: <=0
             if (bmr <= 0) {
                 throw new IllegalArgumentException("El bmr debe ser positivo.");
             }
         } else {
             bmr = 10 * weight + 6.25f * height - 5 * age - 161; 
-            // Restricciones para el bmr
+            // Restricciones para el bmr: <=0
             if (bmr <= 0) {
                 throw new IllegalArgumentException("El bmr debe ser positivo.");
             }
