d3582065c68064bdebf462c3fe56346340b1ed06
JuanSoM <0610948742@uma.es>
2024-03-07 11:39:41 +0100

    basalMetabolicRate

--- src/test/java/healthcalc/HealthCalcImpl.java
+++ src/test/java/healthcalc/HealthCalcImpl.java
@@ -28,6 +28,43 @@ public class HealthCalcImpl {
 
         return idealWeight;
     }
+    public float basalMetabolicRate(float weight, int height, char gender, int age) {
+        // Restriccion para el peso: <=0
+        if (weight <= 0 ) {
+            throw new IllegalArgumentException("El peso debe ser positivo.");
+        }
+       
+        // Restriccion para la altura: <=0
+        if (height <= 0) {
+            throw new IllegalArgumentException("La altura debe ser positiva.");
+        }
+        
+        // Restriccion para la edad: <=0
+        if (age <= 0) {
+            throw new IllegalArgumentException("La edad debe ser positiva.");
+        }
+        // Restricciones para el género
+        if (gender != 'M' && gender != 'F') {
+            throw new IllegalArgumentException("El género debe ser 'M' o 'F'.");
+        }
 
-    
+        // Calcular la Tasa Metabólica Basal (BMR) según la fórmula especificada
+        float bmr;
+        if (Character.toUpperCase(gender) == 'M') {
+            bmr = 10 * weight + 6.25f * height - 5 * age + 5; 
+            // Restricciones para el bmr
+            if (bmr <= 0) {
+                throw new IllegalArgumentException("El bmr debe ser positivo.");
+            }
+        } else {
+            bmr = 10 * weight + 6.25f * height - 5 * age - 161; 
+            // Restricciones para el bmr
+            if (bmr <= 0) {
+                throw new IllegalArgumentException("El bmr debe ser positivo.");
+            }
+
+        }
+
+        return bmr;
+    } 
 }
