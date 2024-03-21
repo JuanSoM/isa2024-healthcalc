fa3c96f4faabfc3e70dcda63c5e61a1efd347683
JuanSoM <0610948742@uma.es>
2024-03-21 09:15:22 +0100

    Clases .java StepDefinition y RunCucumber

--- src/test/java/bdd/StepDefinitions.java
+++ src/test/java/bdd/StepDefinitions.java
@@ -6,8 +6,10 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import healthcalc.HealthCalc;
 import healthcalc.HealthCalcImpl;
+import healthcalc.NegativeValueException;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 
 public class StepDefinitions {
     
@@ -17,6 +19,8 @@ public class StepDefinitions {
     private float result;
     private float weight; 
     private int age; 
+    private boolean raiseException;
+
     
 
     @Before
@@ -29,18 +33,9 @@ public class StepDefinitions {
         weight = 0;
     }
 
-    @Given("^un hombre con una altura de (\\d+) cm$")
+    @Given("que tengo una calculadora para el peso ideal")
     public void un_hombre_con_una_altura_de_cm(int height) {
         this.healthCalc = new HealthCalcImpl();
-        this.height = height;
-        this.gender = 'M';
-    }
-
-    @Given("^una mujer con una altura de (\\d+) cm$")
-    public void una_mujer_con_una_altura_de_cm(int height) {
-        this.healthCalc = new HealthCalcImpl();
-        this.height = height;
-        this.gender = 'F';
     }
 
     @When("^calculo el peso ideal$")
@@ -48,28 +43,48 @@ public class StepDefinitions {
         result = healthCalc.idealWeight(height, gender);
     }
 
-    @Then("^el sistema debería devolver el peso ideal de acuerdo con la formula de Lorentz$")
-    public void el_sistema_debería_devolver_el_peso_ideal_de_acuerdo_con_la_formula_de_Lorentz() {
-        Assert.assertNotEquals(0, result);
+    @Then("el sistema debería devolver el peso ideal de acuerdo con la formula de Lorentz")
+    public void el_sistema_debería_devolver_el_peso_ideal_de_acuerdo_con_la_formula_de_Lorentz(float expected) {
+        Assert.assertNotEquals(expected, result);
     }
 
-    @Given("^un hombre|mujer de (\\d+) años con un peso de (\\d+) kg y una altura de (\\d+) cm$")
+
+
+    @When("Hacer la {int}")
+	public void i_compute_the_factorial_of_a_negative_number(Integer int1) {
+		try {
+			result = factCalc.compute(int1);
+		} catch (NegativeValueException e) {
+			raiseException = true;
+		}
+	}
+    @Then("The system raises an exception")
+	public void the_system_raises_an_exception() {
+	    Assertions.assertTrue(raiseException);
+	}
+
+
+
+
+
+
+
+
+
+
+    @Given("que tengo una calculadora para la tasa metabolica basal")
     public void un_persona_con_datos(int age, float weight, int height) {
         this.healthCalc = new HealthCalcImpl();
-        this.height = height;
-        // Aquí necesitarías establecer el género según el escenario
-        this.gender = 'M';
     }
 
     @When("^calculo la tasa metabólica basal$")
-    public void calculo_la_tasa_metabólica_basal() {
+    public void calculo_la_tasa_metabólica_basal(float weight, int height, char gender, int age) {
         // Aquí necesitarías llamar al método basalMetabolicRate y almacenar el resultado
         result = healthCalc.basalMetabolicRate(weight, height, gender, age);
     }
 
     @Then("^el sistema debería devolver el BMR de acuerdo con la formula especificada$")
-    public void el_sistema_debería_devolver_el_BMR_de_acuerdo_con_la_formula_especificada() {
-        float expectedBMR = 1500; // Cambia este valor al BMR esperado
+    public void el_sistema_debería_devolver_el_BMR_de_acuerdo_con_la_formula_especificada(float expectedBMR ) {
         float delta = 0.001f; // Cambia este valor al margen de error adecuado para tus pruebas
         // Aquí necesitarías agregar las aserciones para verificar si el resultado es el esperado
         Assert.assertEquals(expectedBMR, result, delta);
