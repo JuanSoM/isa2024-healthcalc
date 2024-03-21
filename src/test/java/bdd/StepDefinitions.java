77659d8131e6bce05d14769f23309cd3882b102d
JuanSoM <0610948742@uma.es>
2024-03-21 09:56:24 +0100

    StepDefinitions con los casos de overflow y negativo de todos los parametros numericos

--- src/test/java/bdd/StepDefinitions.java
+++ src/test/java/bdd/StepDefinitions.java
@@ -4,8 +4,8 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import healthcalc.HealthCalc;
 import healthcalc.HealthCalcImpl;
+import healthcalc.IncorrectCharacterException;
 import healthcalc.NegativeValueException;
 
 import org.junit.Assert;
@@ -14,11 +14,7 @@ import org.junit.jupiter.api.Assertions;
 public class StepDefinitions {
     
     private HealthCalcImpl healthCalc;
-    private int height;
-    private char gender;
     private float result;
-    private float weight; 
-    private int age; 
     private boolean raiseException;
 
     
@@ -26,40 +22,70 @@ public class StepDefinitions {
     @Before
     public void initialization() {
         healthCalc = null;
-        height = 0;
-        gender = '\0';
-        result = 0.0f;
-        age = 0;
-        weight = 0;
+        result=0;
+        raiseException=false;
     }
 
+    //PESO IDEAL
+
     @Given("que tengo una calculadora para el peso ideal")
-    public void un_hombre_con_una_altura_de_cm(int height) {
+    public void un_hombre_con_una_altura_de_cm() {
         this.healthCalc = new HealthCalcImpl();
     }
 
-    @When("^calculo el peso ideal$")
-    public void calculo_el_peso_ideal() {
+
+    @When("calculo el peso ideal")
+    public void calculo_el_peso_ideal(int height, char gender) {
         result = healthCalc.idealWeight(height, gender);
     }
 
     @Then("el sistema debería devolver el peso ideal de acuerdo con la formula de Lorentz")
     public void el_sistema_debería_devolver_el_peso_ideal_de_acuerdo_con_la_formula_de_Lorentz(float expected) {
-        Assert.assertNotEquals(expected, result);
+        float delta = 0.001f;
+        Assert.assertEquals(expected, result,delta);
     }
 
 
 
-    @When("Hacer la {int}")
-	public void i_compute_the_factorial_of_a_negative_number(Integer int1) {
+    @When("hacemos el calculo con un altura overflow {int}")
+	public void calculo_el_peso_ideal_con_una_altura_overflow(int height,char gender) {
 		try {
-			result = factCalc.compute(int1);
+			result = healthCalc.idealWeight(height, gender);
 		} catch (NegativeValueException e) {
 			raiseException = true;
 		}
 	}
+    @Then("Devuelve una excepcion de altura overflow")
+	public void ParameterValueCausesOverflow() {
+	    Assertions.assertTrue(raiseException);
+	}
+
+
+
+    @When("hacemos el calculo con un altura negativa {int}")
+	public void calculo_el_peso_ideal_con_una_altura_negativa(int height,char gender) {
+		try {
+			result = healthCalc.idealWeight(height, gender);
+		} catch (NegativeValueException e) {
+			raiseException = true;
+		}
+	}
+    @Then("Devuelve una excepcion de altura negativa")
+	public void devuelve_excepcion_peso() {
+	    Assertions.assertTrue(raiseException);
+	}
+
+
+    @When("hacemos el calculo con un genero incorrecto {char}")
+	public void calculo_el_peso_ideal_con_un_genero_incorrecto(int height,char gender) {
+		try {
+			result = healthCalc.idealWeight(height, gender);
+		} catch (IncorrectCharacterException e) {
+			raiseException = true;
+		}
+	}
     @Then("The system raises an exception")
-	public void the_system_raises_an_exception() {
+	public void devuelve_excepcion_genero() {
 	    Assertions.assertTrue(raiseException);
 	}
 
@@ -72,21 +98,133 @@ public class StepDefinitions {
 
 
 
+
+    //TASA METABOLICA BASAL
+
     @Given("que tengo una calculadora para la tasa metabolica basal")
     public void un_persona_con_datos(int age, float weight, int height) {
         this.healthCalc = new HealthCalcImpl();
     }
 
-    @When("^calculo la tasa metabólica basal$")
+
+    
+
+    @When("calculo la tasa metabólica basal")
     public void calculo_la_tasa_metabólica_basal(float weight, int height, char gender, int age) {
         // Aquí necesitarías llamar al método basalMetabolicRate y almacenar el resultado
         result = healthCalc.basalMetabolicRate(weight, height, gender, age);
     }
 
-    @Then("^el sistema debería devolver el BMR de acuerdo con la formula especificada$")
+    @Then("el sistema debería devolver el BMR de acuerdo con la formula especificada")
     public void el_sistema_debería_devolver_el_BMR_de_acuerdo_con_la_formula_especificada(float expectedBMR ) {
         float delta = 0.001f; // Cambia este valor al margen de error adecuado para tus pruebas
         // Aquí necesitarías agregar las aserciones para verificar si el resultado es el esperado
         Assert.assertEquals(expectedBMR, result, delta);
     }
+    
+    //ALTURA OVERFLOW
+
+    @When("hacemos el calculo con un altura overflow {int}")
+	public void calculo_la_tasa_metabólica_basal_con_una_altura_overflow(float weight, int height, char gender, int age) {
+		try {
+			result = healthCalc.basalMetabolicRate(weight, height, gender, age);
+		} catch (NegativeValueException e) {
+			raiseException = true;
+		}
+	}
+    @Then("Devuelve una excepcion de altura overflow")
+	public void ParameterValueCausesOverflow_BMR() {
+	    Assertions.assertTrue(raiseException);
+	}
+
+    //ALTURA NEGATIVA
+
+    @When("hacemos el calculo con un altura negativa {int}")
+	public void calculo_la_tasa_metabólica_basal_con_una_altura_negativa(float weight, int height, char gender, int age) {
+		try {
+			result = healthCalc.basalMetabolicRate(weight, height, gender, age);
+		} catch (NegativeValueException e) {
+			raiseException = true;
+		}
+	}
+    @Then("Devuelve una excepcion de altura negativa")
+	public void devuelve_excepcion_altura_BMR() {
+	    Assertions.assertTrue(raiseException);
+	}
+
+    //PESO OVERFLOW
+    @When("hacemos el calculo con un peso overflow {int}")
+	public void calculo_la_tasa_metabólica_basal_con_un_peso_overflow(float weight, int height, char gender, int age) {
+		try {
+			result = healthCalc.basalMetabolicRate(weight, height, gender, age);
+		} catch (NegativeValueException e) {
+			raiseException = true;
+		}
+	}
+    @Then("Devuelve una excepcion de peso overflow")
+	public void ParameterValueCausesOverflow_BMR_peso() {
+	    Assertions.assertTrue(raiseException);
+	}
+
+    //PESO NEGATIVO
+
+    @When("hacemos el calculo con un peso negativo {int}")
+	public void calculo_la_tasa_metabólica_basal_con_un_peso_negativo(float weight, int height, char gender, int age) {
+		try {
+			result = healthCalc.basalMetabolicRate(weight, height, gender, age);
+		} catch (NegativeValueException e) {
+			raiseException = true;
+		}
+	}
+    @Then("Devuelve una excepcion de peso negativo")
+	public void devuelve_excepcion_peso_BMR() {
+	    Assertions.assertTrue(raiseException);
+	}
+
+    //EDAD OVERFLOW
+    @When("hacemos el calculo con una edad overflow {int}")
+	public void calculo_la_tasa_metabólica_basal_con_una_edad_overflow(float weight, int height, char gender, int age) {
+		try {
+			result = healthCalc.basalMetabolicRate(weight, height, gender, age);
+		} catch (NegativeValueException e) {
+			raiseException = true;
+		}
+	}
+    @Then("Devuelve una excepcion de peso overflow")
+	public void ParameterValueCausesOverflow_BMR_edad() {
+	    Assertions.assertTrue(raiseException);
+	}
+
+    //EDAD NEGATIVA
+
+    @When("hacemos el calculo con una edad negativa {int}")
+	public void calculo_la_tasa_metabólica_basal_con_una_edad_negativa(float weight, int height, char gender, int age) {
+		try {
+			result = healthCalc.basalMetabolicRate(weight, height, gender, age);
+		} catch (NegativeValueException e) {
+			raiseException = true;
+		}
+	}
+    @Then("Devuelve una excepcion de edad negativa")
+	public void devuelve_excepcion_edad_BMR() {
+	    Assertions.assertTrue(raiseException);
+	}
+
+
+    //GENERO INCORRECTO
+    @When("hacemos el calculo con un genero incorrecto {char}")
+	public void calculo_la_tasa_metabólica_basal_con_un_genero_incorrecto(float weight, int height, char gender, int age) {
+		try {
+			result = healthCalc.basalMetabolicRate(weight, height, gender, age);
+		} catch (IncorrectCharacterException e) {
+			raiseException = true;
+		}
+	}
+    @Then("The system raises an exception")
+	public void devuelve_excepcion_genero_BMR() {
+	    Assertions.assertTrue(raiseException);
+	}
+
+
+
 }
