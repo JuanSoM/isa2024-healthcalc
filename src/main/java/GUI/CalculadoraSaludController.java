3c6051c5cce060d5e75f36fc99941d3aea44d28b
JuanSoM <0610948742@uma.es>
2024-05-19 15:06:13 +0200

    Adaptamos el controlador y adapter a la refactorizacion

--- src/main/java/GUI/CalculadoraSaludController.java
+++ src/main/java/GUI/CalculadoraSaludController.java
@@ -4,16 +4,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import healthcalc.HealthCalcImpl;
+import healthcalc.PersonImpl;
+import healthcalc.Gender;
 
 public class CalculadoraSaludController {
 
     private CalculadoraSaludView vista;
-    private HealthCalcImpl model= HealthCalcImpl.getInstance();
+    private HealthCalcImpl model = HealthCalcImpl.getInstance();
 
     public CalculadoraSaludController(CalculadoraSaludView vista, HealthCalcImpl model) {
         this.vista = vista;
         this.model = model;
-        
+
         // Suscribir el controlador a los eventos de los botones en la vista
         vista.getBtnCalcularPesoIdeal().addActionListener(new ActionListener() {
             @Override
@@ -33,8 +35,12 @@ public class CalculadoraSaludController {
     private void calcularPesoIdeal() {
         try {
             int altura = Integer.parseInt(vista.getTxtAltura().getText());
-            char genero = vista.getComboBoxGenero().getSelectedItem().toString().charAt(0);
-            float pesoIdeal = model.idealWeight(altura, genero);
+            String generoStr = vista.getComboBoxGenero().getSelectedItem().toString();
+            Gender genero = generoStr.equalsIgnoreCase("M") ? Gender.MALE : Gender.FEMALE;
+            
+            // Crear un objeto PersonImpl solo con altura y género
+            PersonImpl persona = new PersonImpl(altura, genero);
+            double pesoIdeal = model.getIdealBodyWeight(persona);
             vista.mostrarMensaje("Peso Ideal: " + pesoIdeal + " kg");
         } catch (NumberFormatException ex) {
             vista.mostrarMensaje("Por favor, ingrese valores numéricos válidos para la altura.");
@@ -48,8 +54,12 @@ public class CalculadoraSaludController {
             float peso = Float.parseFloat(vista.getTxtPeso().getText());
             int altura = Integer.parseInt(vista.getTxtAltura().getText());
             int edad = Integer.parseInt(vista.getTxtEdad().getText());
-            char genero = vista.getComboBoxGenero().getSelectedItem().toString().charAt(0);
-            float bmr = model.basalMetabolicRate(peso, altura, genero, edad);
+            String generoStr = vista.getComboBoxGenero().getSelectedItem().toString();
+            Gender genero = generoStr.equalsIgnoreCase("M") ? Gender.MALE : Gender.FEMALE;
+            
+            // Crear un objeto PersonImpl con todos los atributos
+            PersonImpl persona = new PersonImpl(peso, altura, genero, edad);
+            double bmr = model.basalMetabolicRate(persona);
             vista.mostrarMensaje("BMR: " + bmr + " kcal/día");
         } catch (NumberFormatException ex) {
             vista.mostrarMensaje("Por favor, ingrese valores numéricos válidos para peso, altura y edad.");
--- src/main/java/healthcalc/CalculatorHealthAdapter.java
+++ src/main/java/healthcalc/CalculatorHealthAdapter.java
@@ -1,23 +1,30 @@
 package healthcalc;
 
-//Adaptador que implementa la interfaz HealthHospital y utiliza la clase HealthCalcImpl
+// Adaptador que implementa la interfaz HealthHospital y utiliza la clase HealthCalcImpl
 public class CalculatorHealthAdapter implements HealthHospital {
- private HealthCalcImpl healthCalculator;
+    private HealthCalcImpl healthCalculator;
 
- // Constructor que recibe una instancia de HealthCalcImpl
- public CalculatorHealthAdapter(HealthCalcImpl healthCalculator) {
-     this.healthCalculator = healthCalculator;
- }
+    // Constructor que recibe una instancia de HealthCalcImpl
+    public CalculatorHealthAdapter(HealthCalcImpl healthCalculator) {
+        this.healthCalculator = healthCalculator;
+    }
 
- // Implementación del método bmr de la interfaz HealthHospital
- @Override
- public double bmr(char gender, int age, int height, float weight) {
-     return healthCalculator.basalMetabolicRate(weight, height, gender, age);
- }
+    // Implementación del método bmr de la interfaz HealthHospital
+    @Override
+    public double bmr(char gender, int age, int height, float weight) {
+        // Convertimos el char gender a Gender enum y creamos un objeto Person
+        Gender genderEnum = Character.toUpperCase(gender) == 'M' ? Gender.MALE : Gender.FEMALE;
+        Person person = new PersonImpl(weight, height, genderEnum, age);
+        return healthCalculator.basalMetabolicRate(person);
+    }
 
- // Implementación del método pesoIdeal de la interfaz HealthHospital
- @Override
- public float pesoIdeal(char gender, int height) {
-     return healthCalculator.idealWeight(height, gender);
- }
+    // Implementación del método pesoIdeal de la interfaz HealthHospital
+    @Override
+    public float pesoIdeal(char gender, int height) {
+        // Convertimos el char gender a Gender enum y creamos un objeto Person
+        Gender genderEnum = Character.toUpperCase(gender) == 'M' ? Gender.MALE : Gender.FEMALE;
+        // El peso y la edad no son relevantes para este cálculo, se usan valores arbitrarios válidos
+        Person person = new PersonImpl(height, genderEnum);
+        return (float) healthCalculator.getIdealBodyWeight(person);
+    }
 }
